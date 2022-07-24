b014b1e5cd7e197a34f7580437a2c38f187d821f
yousef <gheibiyousef2000@gmail.com>
2022-07-24 22:32:09 +0430

    some change on clearity of code

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,35 +4,21 @@ const db = require('../db');
 const router = express.Router();
 
 const jwt = require("jsonwebtoken");
-var auth = require("../services/auth");
 
-
-router.get('/getByEmail/:id', (req, res) => {
-    const id = req.params.id;
-    let query = `select * from user where email = ?`;
-    db.query(query, [id], (err, result) => {
-        if (!err) {
-            return res.status(200).json(result[0]);
-        } else {
-            return res.status(500).json(err);
-        }
-    })
-})
 
 router.post('/login', (req, res) => {
     const user = req.body;
-    let query = `select email,password from user where email=?`;
+    let query = `select * from user where email=?`;
     db.query(query, [user.email], (err, result) => {
         if (!err) {
-            console.log(result);
             if (result.length <= 0 || result[0].password != user.password) {
-                return res.status(401).json({ message: "Incorrect Username or password" });
+                return res.status(401).json({ message: "ایمیل یا رمز عبور نادرست است !" });
             } else if (result[0].password == user.password) {
                 const response = { email: result[0].email }
                 const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' });
-                res.status(200).json({ message: "Successfully logined.", token: accessToken, id: result[0].id });
+                res.status(200).json({ message: "ورود موفقیت آمیز بود.", token: accessToken, id: result[0].id });
             } else {
-                return res.status(400).json({ message: "Something went wrong. Please try again later." });
+                return res.status(400).json({ message: "اشتباهی رخ داده ، لطفاً بعداً دوباره امتحان کنید." });
             }
         } else {
             return res.status(500).json(err);
@@ -40,10 +26,6 @@ router.post('/login', (req, res) => {
     })
 
 })
-
-
-
-
 
 
 
